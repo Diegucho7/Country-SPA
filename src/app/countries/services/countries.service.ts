f6eecc4ff8dee458d1c34e94c6fc6ed7335b253e
Diego <dvelarde140@gmail.com>
2023-07-10 16:32:48 -0500

    manejo de errores

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -14,7 +14,10 @@ export class CountriesService {
 
     searchCapital( term : string): Observable<Country[]>{
         const url = `${this.apiUrl}/capital/${term}`;
-        return this.http.get<Country[]>(url);
+        return this.http.get<Country[]>(url)
+        .pipe(
+            catchError(error => of([]))
+        );
 
     }
 }
