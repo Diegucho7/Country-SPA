dfbc1ea632a4b27993e328b429a1835a2f084d0d
Diego <dvelarde140@gmail.com>
2023-07-13 17:06:10 -0500

    renderizacion de codigo

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -12,11 +12,12 @@ export class CountriesService {
     constructor(private http: HttpClient) { }
     
 
-    searchCountryByAlphaCode( code : string): Observable<Country[]> {
+    searchCountryByAlphaCode( code : string): Observable<Country | null> {
         const url = `${this.apiUrl}/alpha/${code}`;
         return this.http.get<Country[]>(url)
         .pipe(
-            catchError(error => of([]))
+            map(countries => countries.length > 0 ? countries[0]: null),
+            catchError(() => of(null))
         );
         
 
