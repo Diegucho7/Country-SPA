048517eaf0528c44bcddc139e0ed2df790e63f0b
Diego <dvelarde140@gmail.com>
2023-07-31 15:14:52 -0500

    optimizacion en pagina de region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component, Output } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania'
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -12,10 +14,15 @@ export class ByRegionPageComponent {
 
     public countries: Country[]= [];
 
+    public regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania'];
+    public selectedRegion?: Region;
     constructor (private countriesService: CountriesService){}
     
-  searchByRegion (term: string):void{
-    this.countriesService.searchByRegion(term)
+  searchByRegion (region: Region):void{
+
+    this.selectedRegion = region;
+
+    this.countriesService.searchByRegion(region)
     .subscribe(countries => {
       this.countries = countries;
     })
